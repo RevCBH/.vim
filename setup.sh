7638f17e26c00b2e1b99d953fabddab229778b00
Bennett Hoffman <benn.hoffman@gmail.com>
2013-04-15 10:19:22 -0700

    simple setup file

---
+++ setup.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+
+cp ./.vimrc ~/.vimrc
+cp -R ./.vim/after/ ~/.vim/after
+cp -R ./.vim/autoload/ ~/.vim/autoload
+cp -R ./.vim/bundle/ ~/.vim/bundle
+cp -R ./.vim/colors/ ~/.vim/colors
